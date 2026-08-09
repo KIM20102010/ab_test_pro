fd41b8613e56bf8110d87dda2dd2f9d27767b54c
KIM20102010 <767717364@qq.com>
2026-08-10 00:38:47 +0800

    Fix formatting issue in download button code

--- app_v2.py
+++ app_v2.py
@@ -880,7 +880,7 @@ if st.session_state.batch_files and st.session_state.user_plan in ['starter', 'f
                     label="⬇️ Download ZIP",
                     data=zip_buffer,
                     file_name=f"ABTest_Batch_{datetime.now().strftime('%Y%m%d')}.zip",
-                    mime="application/zip"
+                    mime="application/zip",
                     key=f"download_pdf_{btn_key}"
                 )
     
